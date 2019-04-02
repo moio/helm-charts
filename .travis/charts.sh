a9d57b9ad4f335edf7f93c8f6bdbfb5e852cff26
Sergey Anisimov <sanisimov@moikot.com>
2019-04-02 22:42:24 +0200

    Fix folder

--- .travis/charts.sh
+++ .travis/charts.sh
@@ -18,7 +18,7 @@ publish() {
   printf "User-Agent: *\nDisallow: /\n" > ./public/robots.txt
 
   docker run -it --rm -v $(pwd):/repo --entrypoint /bin/sh linkyard/docker-helm \
-  -c "helm init --client-only && helm package /repo/charts/* --destination /repo/public && cd ./public && helm repo index --url ${url} ."
+  -c "helm init --client-only && helm package /repo/charts/* --destination /repo/public && cd /repo/public && helm repo index --url ${url} ."
 #  helm init --client-only
 #  helm package charts/* --destination ./public
 #  cd ./public
