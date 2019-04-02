fb4ae94929d78c35d8adf1336a9d1bbcc48bb5c4
Sergey Anisimov <sanisimov@moikot.com>
2019-04-02 22:39:20 +0200

    Create charts

--- .travis/charts.sh
+++ .travis/charts.sh
@@ -5,11 +5,24 @@ set -e
 #
 # Publishes Helm charts.
 #
+# $1 - The charts URL.
+#
 # Examples:
 #
-#   publish
+#   publish "https://moikot.github.io/helm-charts"
 #
 publish() {
+  readonly url="${1}"
+
+  mkdir -p ./public
+  printf "User-Agent: *\nDisallow: /\n" > ./public/robots.txt
+
+  docker run -it --rm -v $(pwd):/repo --entrypoint /bin/sh linkyard/docker-helm \
+  -c "helm init --client-only && helm package /repo/charts/* --destination /repo/public && cd ./public && helm repo index --url ${url} ."
+#  helm init --client-only
+#  helm package charts/* --destination ./public
+#  cd ./public
+#  helm repo index --url "${url}" .
 }
 
 "$@"
